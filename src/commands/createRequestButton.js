0bde63d6a9ea6897b39b9f4ea3636db79d8db611
l.forzutti <l.forzutti@egeatech.com>
2023-04-23 17:55:45 +0200

    build(elements) create button request

--- src/commands/createRequestButton.js
+++ src/commands/createRequestButton.js
@@ -1,1 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
+const accessRequestButton = require('../components/activateModal').data;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('createRequestButton')
+    .setDescription('Creates a request button inside current channel')
+    .setDefaultMemberPermissions(0),
+  async execute(interaction) {
+    console.log(`Handling createRequestButton command for ${interaction?.user?.tag}`);
+
+    await interaction.deferReply();
+    await interaction.channel.send({ components: [accessRequestButton] });
+    await interaction.deleteReply();
+  },
+};
