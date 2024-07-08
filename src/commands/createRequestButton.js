c3e7fb4a3c6e0185ad0b5b5885bcb058be66058d
Luca <luca@ahd-creative.agency>
2024-07-08 17:59:58 +0200

    refactor(commands) create request

--- src/commands/createRequestButton.js
+++ src/commands/createRequestButton.js
@@ -1,16 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 const accessRequestButton = require('../components/activateModal').data;
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('createRequestButton')
-    .setDescription('Creates a request button inside current channel')
+    .setName('requestaccessity')
+    .setDescription('Creates a request access button in the current channel.')
     .setDefaultMemberPermissions(0),
   async execute(interaction) {
-    console.log(`Handling createRequestButton command for ${interaction?.user?.tag}`);
+    console.log(`Handling createRequestButton command for ${interaction?.user?.tag}`)
 
     await interaction.deferReply();
-    await interaction.channel.send({ components: [accessRequestButton] });
+    await interaction.channel.send({ components: [ accessRequestButton ] });
     await interaction.deleteReply();
-  },
-};
+  }
+}
