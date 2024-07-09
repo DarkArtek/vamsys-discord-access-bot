0b658c381835bc34ba58033dadb698e2243fd603
pokertour <guillaume_35720@hotmail.fr>
2024-07-09 12:58:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const DiscordBot = require('./src');
 const { DISCORD_TOKEN, BOT_SERVERS, NICK_FORMAT } = require('./src/constants/configuration');
 
+var configFile = require('../../config/config.json');
+
 async function start() {
   try {
+		console.log(configFile.nickFormat);
 		console.log(NICK_FORMAT);
     new DiscordBot(DISCORD_TOKEN, BOT_SERVERS);
   } catch(e) {
