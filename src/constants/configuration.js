1c5bf9e20e8fc6b11945705bb59038a44e3e1f57
pokertour <guillaume_35720@hotmail.fr>
2024-07-09 10:20:20 +0200

    Update configuration.js

--- src/constants/configuration.js
+++ src/constants/configuration.js
@@ -9,12 +9,16 @@ const DISCORD_TOKEN = configFile.discordToken;
 // Servers from the config.json file
 const BOT_SERVERS = configFile.servers;
 
-// Servers from the config.json file
+// AirlineID from the config.json file
 const AIRLINE_ID = configFile.airlineId;
+
+// Nickname Format from the config.json file
+const NICK_FORMAT = configFile.nickFormat;
 
 module.exports = {
   VAMSYS_API_KEY,
   DISCORD_TOKEN,
   BOT_SERVERS,
   AIRLINE_ID,
+	NICK_FORMAT,
 }
